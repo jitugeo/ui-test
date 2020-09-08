946d0593324ce963d92663bb6a776839fae2ed50
JITENDER KAUSHIK <15875919+jitugeo@users.noreply.github.com>
2020-09-08 02:00:55 -0400

    unsubscribing the subscription.

--- albums/src/app/app.component.ts
+++ albums/src/app/app.component.ts
@@ -1,31 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from './services/api.service';
 import { Song, songWithInfo } from './models/songs';
+import { filter, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  songs: songWithInfo[];
-  pages:Array<any>;
+export class AppComponent implements OnInit, OnDestroy {
+	songs: songWithInfo[];
+	pages: Array<any>;
+	private dataUnsubscribe$: Subject<void> = new Subject();
 
-  constructor(private apiService:ApiService){}  
 
-  ngOnInit(){
-    this.apiService.getInitData()
-    .subscribe((songsResp:{songs:songWithInfo[],links:Array<any>})=> {
-      this.songs = songsResp.songs;
-      this.pages = songsResp.links;
-    })
-  }
+	constructor(private apiService: ApiService) { }
 
-  paginationClick(url){
-    this.apiService.getSongs(url)
-    .subscribe((songsResp:{songs:songWithInfo[],links:Array<any>})=> {
-      this.songs = songsResp.songs;
-      this.pages = songsResp.links;
-    })
-  }
+
+	ngOnInit() {
+		this.apiService.getInitData()
+			.pipe(
+				filter(data => data !== undefined),
+				takeUntil(this.dataUnsubscribe$)
+			)
+			.subscribe((songsResp: { songs: songWithInfo[], links: Array<any> }) => {
+				this.songs = songsResp.songs;
+				this.pages = songsResp.links;
+			});
+	}
+
+	paginationClick(url) {
+		this.apiService.getSongs(url)
+			.pipe(
+				filter(data => data !== undefined),
+				takeUntil(this.dataUnsubscribe$)
+			)
+			.subscribe((songsResp: { songs: songWithInfo[], links: Array<any> }) => {
+				this.songs = songsResp.songs;
+				this.pages = songsResp.links;
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.dataUnsubscribe$.next();
+		this.dataUnsubscribe$.complete();
+	}
+
 }
